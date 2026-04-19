2ae74321435589f1f9503e2395d7d04abc84e6aa
melmel <carmelon2010@gmail.com>
2026-04-19 13:07:27 +0300

    Add spear lunge and mace attack key bindings with handling logic

--- src/client/java/me/carmel/sprint/client/SprintClient.java
+++ src/client/java/me/carmel/sprint/client/SprintClient.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import org.lwjgl.glfw.GLFW;
 import net.minecraft.entity.player.PlayerInventory;
@@ -12,76 +13,117 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 
 public class SprintClient implements ClientModInitializer {
-    private static KeyBinding extraSprintKey;
+    private static KeyBinding SpearLunge;
     private int lastSelectedSlot = -1;
     private boolean isattacking = false;
+    private static KeyBinding MaceAttack;
 
     @Override
     public void onInitializeClient() {
-        extraSprintKey = KeyBindingHelper.registerKeyBinding(
-                new KeyBinding("sprint v2", GLFW.GLFW_KEY_R, KeyBinding.Category.MOVEMENT)
+        SpearLunge = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding("spear dash", GLFW.GLFW_KEY_UNKNOWN, KeyBinding.Category.MOVEMENT)
+        );
+        MaceAttack = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding("Mace atttack", GLFW.GLFW_KEY_UNKNOWN, KeyBinding.Category.MOVEMENT)
         );
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player == null) {
-                return;
-            }
+        ClientTickEvents.END_CLIENT_TICK.register(this::handleClientTick);
+    }
 
-            if (isattacking){
-                System.out.println("returning to " + lastSelectedSlot);
-                isattacking = false;
-                if (lastSelectedSlot != -1) {
-                    client.player.getInventory().setSelectedSlot(lastSelectedSlot);
-                }
-                return;
-            }
-            if (client.attackCooldown > 0) {
-                return;
-            } else if (client.crosshairTarget == null) {
-                Logger LOGGER = LogUtils.getLogger();
-                LOGGER.error("Null returned as 'hitResult', this shouldn't happen!");
-                if (client.interactionManager.hasLimitedAttackSpeed()) {
-                    client.attackCooldown = 10;
+    private void handleClientTick(MinecraftClient client) {
+        if (client.player == null) {
+            return;
+        }
+
+        if (restoreSlotIfNeeded(client) || shouldSkipAttack(client)) {
+            return;
+        }
+
+        if (!SpearLunge.wasPressed()) {
+            PlayerInventory inventory = client.player.getInventory();
+            for (int i = 0; i < inventory.getHotbarSize(); i++) {
+                ItemStack stack = inventory.getStack(i);
+                if (!isEligibleSpear(stack)) {
+                    continue;
                 }
 
-                return;
-            }
-
-            if (extraSprintKey.wasPressed()) {
-                PlayerInventory inventory = client.player.getInventory();
-
-                for (int i = 0; i < inventory.getHotbarSize(); i++) {
-                    ItemStack stack = inventory.getStack(i);
-                    if (stack.isEmpty()) {
-                        continue;
-                    }
-//                    System.out.println("Slot " + i + ": " + stack.getName().getString() + " x" + stack.getCount());
-                    if (!stack.getItemName().getString().toLowerCase().contains("spear")) {
-                        continue;
-                    }
-
-                    isattacking = true;
+                if (!isattacking) {
                     lastSelectedSlot = inventory.getSelectedSlot();
-                    System.out.println("moved from" + lastSelectedSlot);
+                }
+                isattacking = true;
+                System.out.println("moved from" + inventory.getSelectedSlot());
 
-                    inventory.setSelectedSlot(i);
-
-                    // put jab attack here
-                    if (client.interactionManager != null) {
-                        client.player.swingHand(Hand.MAIN_HAND);
-
-                        KeyBinding.setKeyPressed(client.options.attackKey.getDefaultKey(), true);
-                        KeyBinding.onKeyPressed(client.options.attackKey.getDefaultKey());
-
-                        client.player.swingHand(Hand.MAIN_HAND);
-
-                        KeyBinding.setKeyPressed(client.options.attackKey.getDefaultKey(), false);
-                    }
-
-
+                inventory.setSelectedSlot(i);
+                triggerMainHandAttack(client);
+            }
+        } else if (MaceAttack.wasPressed()) {
+            PlayerInventory inventory = client.player.getInventory();
+            for (int i = 0; i < inventory.getHotbarSize(); i++) {
+                ItemStack stack = inventory.getStack(i);
+                if (!isEligibleMace(stack)) {
+                    continue;
                 }
 
+                if (!isattacking) {
+                    lastSelectedSlot = inventory.getSelectedSlot();
+                }
+                isattacking = true;
+                System.out.println("moved from" + inventory.getSelectedSlot());
+
+                inventory.setSelectedSlot(i);
+                triggerMainHandAttack(client);
             }
-        });
+
+        }
+
+    }
+
+    private boolean restoreSlotIfNeeded(MinecraftClient client) {
+        if (!isattacking) {
+            return false;
+        }
+
+        System.out.println("returning to " + lastSelectedSlot);
+        isattacking = false;
+        if (lastSelectedSlot != -1) {
+            client.player.getInventory().setSelectedSlot(lastSelectedSlot);
+        }
+        return true;
+    }
+
+    private boolean shouldSkipAttack(MinecraftClient client) {
+        if (client.attackCooldown > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isEligibleSpear(ItemStack stack) {
+        if (stack.isEmpty()) {
+            return false;
+        }
+
+        return stack.getItemName().getString().toLowerCase().contains("spear")
+                && stack.getEnchantments().getEnchantments().contains("minecraft:lunge");
+    }
+
+    private boolean isEligibleMace(ItemStack stack) {
+        if (stack.isEmpty()) {
+            return false;
+        }
+
+        return stack.getItemName().getString().toLowerCase().contains("mace");
+    }
+
+    private void triggerMainHandAttack(MinecraftClient client) {
+        if (client.interactionManager == null) {
+            return;
+        }
+
+        client.player.swingHand(Hand.MAIN_HAND);
+        KeyBinding.setKeyPressed(client.options.attackKey.getDefaultKey(), true);
+        KeyBinding.onKeyPressed(client.options.attackKey.getDefaultKey());
+        client.player.swingHand(Hand.MAIN_HAND);
+        KeyBinding.setKeyPressed(client.options.attackKey.getDefaultKey(), false);
     }
 }
